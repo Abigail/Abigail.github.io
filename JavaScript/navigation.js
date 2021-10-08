9e752e7c2b13e0a4ed54236d66349c57c2367ab1
Abigail <abigail@abigail.be>
2021-10-08 19:00:29 +0200

    Blog for week 133, part 2

--- JavaScript/navigation.js
+++ JavaScript/navigation.js
@@ -68,6 +68,7 @@ let pwc_pages = [
     ["week-132-1", "Mirror Dates"],
     ["week-132-2", "Hash Join"],
     ["week-133-1", "Integer Square Root"],
+    ["week-133-2", "Smith Numbers"],
 ]
 
 pwc_pages . forEach (_ => _ [0] = PWC + "/" + _ [0] + ".html")
