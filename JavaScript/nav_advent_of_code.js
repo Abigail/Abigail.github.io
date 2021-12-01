af6e479bd14587f7db3b90b29c11db4bd8edcb45
Abigail <abigail@abigail.be>
2021-12-01 19:30:11 +0100

    Handle generating indices

--- JavaScript/nav_advent_of_code.js
+++ JavaScript/nav_advent_of_code.js
@@ -8,7 +8,11 @@ let pages = [
     ["day-01", "Sonar Sweep"],
 ]
 
-pages . forEach (_ => _ [0] = dir + "/" + _ [0] + ".html")
+pages . forEach (entry => {
+    let number = entry [0] . substr (4, 5)
+    entry [0]  = dir + "/" + entry [0] + ".html"
+    entry [1]  = `Day ${number}: ` + entry [1]
+})
 
 window . pages = pages
 window . dir   = dir
