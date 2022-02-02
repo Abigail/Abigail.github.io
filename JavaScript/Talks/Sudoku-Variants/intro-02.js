0ea9c31c5277b969c2a4d9756c7be8d29c004f27
Abigail <abigail@abigail.freedom.nl>
2022-02-02 17:18:08 +0100

    Second page: define some terms.

--- JavaScript/Talks/Sudoku-Variants/intro-02.js
+++ JavaScript/Talks/Sudoku-Variants/intro-02.js
@@ -1,7 +1,38 @@
 $(document) . ready (function () {
     let sudoku = new Sudoku ({})
 
+    $("li") . addClass ("hidden")
+
     sudoku . draw ()
+
+    add_todo (() => {unhide ()})
+
+    // Rows
+    add_todo (() => {
+        unhide ()
+        sudoku . show_house ("R3")
+    })
+
+    // Columns
+    add_todo (() => {
+        unhide ()
+        sudoku . clear_houses ()
+        sudoku . show_house ("C7")})
+
+    // Boxes
+    add_todo (() => {
+        unhide ()
+        sudoku . clear_houses ()
+        sudoku . show_house ("B2")
+    })
+
+    // Cell
+    add_todo (() => {
+        unhide ()
+        sudoku . clear_houses ()
+        sudoku . show_house ("#R5C5")
+    })
+    
 })
 
 
--- Src/Talks/Sudoku-Variants/intro-02.md
+++ Src/Talks/Sudoku-Variants/intro-02.md
@@ -1,4 +1,10 @@
-# Sudoku
+# Sudoku: Some terms
 
 <!-- %% svg-grid: left -->
 
+* Houses or Regions
+    * Rows
+    * Columns
+    * Boxes
+
+* Cells
