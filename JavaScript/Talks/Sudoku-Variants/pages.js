285780c1f7cd96791991c5b87432a54816a9640d
Abigail <abigail@abigail.freedom.nl>
2022-02-06 22:22:33 +0100

    Sudoku: First page about regexp construction.

--- JavaScript/Talks/Sudoku-Variants/pages.js
+++ JavaScript/Talks/Sudoku-Variants/pages.js
@@ -1,11 +1,12 @@
 let dir = "Talks/Sudoku-Variants"
 
 let pages = [
-    ["intro-01",    "Sudoku Introduction"],
-    ["intro-02",    "Sudoku Naming"],
-    ["intro-04",    "Sudoku by Regexp"],
-    ["intro-05",    "Sudoku by Regexp"],
-    ["intro-06",    "Regexp Limitations"],
+    ["intro-01",         "Sudoku Introduction"],
+    ["intro-02",         "Sudoku Naming"],
+    ["intro-04",         "Sudoku by Regexp"],
+    ["intro-05",         "Sudoku by Regexp"],
+    ["intro-06",         "Regexp Limitations"],
+    ["construction-01",  "Constructing a pattern"],
 ]
 
 pages . forEach (entry => {
---
+++ Src/Talks/Sudoku-Variants/construction-01.md
@@ -0,0 +1,9 @@
+# Constructing a Pattern
+
+<!-- %% svg-grid: none -->
+
+* Start with an empty subject and pattern
+* Process the cells one-by-one
+* For each cell:
+    * Add something to the subject
+    * Add something to the pattern
