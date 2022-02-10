2cb64846e722c805cd7280ae8dc7f823fe282350
Abigail <abigail@abigail.freedom.nl>
2022-02-10 20:00:21 +0100

    Sudoku: Improve page with initial subject and pattern.
    * We added some CSS to justify the content.
    * We make the content div visible only after we've calculated the fontsize.
    * We show the subject and pattern in two different colours.

--- JavaScript/Talks/Sudoku-Variants/construction-06.js
+++ JavaScript/Talks/Sudoku-Variants/construction-06.js
@@ -7,17 +7,28 @@ function font_fiddle (selection) {
         return;
     }
 
+ // element . style . visibility = "hidden"
     element . style . fontSize = fontSize + 'px';
     while (element . scrollHeight > element . clientHeight && fontSize > 0) {
         element . style . fontSize  = -- fontSize + 'px';
     }
+ // element . style . visibility = "visible"
 }
 
 
 $(document) . ready (function () {
+    let element = $(".content")
+    element . css ({visibility: "hidden"})
+    let text = element . html ()
+        text = text . replace ('"', '"' + "<span class = 'subject'>")
+        text = text . replace ('"=~/', '</span>"=~/<span class = "pattern">')
+        text = text . replace ("$/", "</span>/")
+    element . html (text)
     font_fiddle (".content")
+    element . css ({visibility: "visible"})
 })
 
 $(window) . resize (() => {
+    $(".content") . css ({visibility: "hidden"})
     font_fiddle (".content")
 })
