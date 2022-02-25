db2cd2905a742493b43ffa863bee3556ecd47bc8
Abigail <abigail@abigail.freedom.nl>
2022-02-25 14:11:49 +0100

    Sudoku: Show solution in 4x4 variant.

--- JavaScript/Talks/Sudoku-Variants/size-4-1.js
+++ JavaScript/Talks/Sudoku-Variants/size-4-1.js
@@ -6,6 +6,12 @@ $(document) . ready (function () {
                                  [3, 0, 0, 1],
                                  [0, 0, 4, 0]]})
 
+    sudoku . set_solution ({solution: [[4, 3, 1, 2],
+                                       [1, 2, 3, 4],
+                                       [3, 4, 2, 1],
+                                       [2, 1, 4, 3]]})
+
     sudoku . draw ()
     sudoku . draw_clues ()
+    add_todo (() => sudoku . draw_solution ({delay: 100}))
 })
