5cf712b8719d3f87eb7174777c0101f06a5cf07c
Abigail <abigail@abigail.freedom.nl>
2022-03-01 22:54:23 +0100

    Sudoku: Remove outcommented code.

--- JavaScript/Talks/Sudoku-Variants/asterisk-1.js
+++ JavaScript/Talks/Sudoku-Variants/asterisk-1.js
@@ -2,9 +2,4 @@ $(document) . ready (function () {
     let sudoku = new Sudoku ({size: 9, asterisk: 1})
 
     sudoku . draw ()
-
-//  add_todo (() => {
-//      $("div.asterisk") . html ("<span class = 'sign'>&#8251;</span>")
-//      $("div.asterisk") . css  ({"margin-top": "5vh"})
-//  })
 })
