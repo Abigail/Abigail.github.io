d364274365c279f55268685403e711b23c7f9395
Abigail <abigail@abigail.freedom.nl>
2022-05-11 01:12:34 +0200

    Trapped: Easier handling for moves.
    - Fold moves into 8 or 4 directions
    - step/slide helper functions
    - move_list in parent class

--- JavaScript/Trapped/pieces.js
+++ JavaScript/Trapped/pieces.js
@@ -1,48 +1,90 @@
 //
 // Chess pieces go here
 //
+
+//
+// Return a move representing a step
+//
+function step (dr, dc, args = {}) {
+    return {type: "step", dr: dr, dc: dc}
+}
+
+//
+// Return a move representing a slide
+//
+function slide (dr, dc, args = {}) {
+    return {type: "slide", dr: dr, dc: dc}
+}
+
+//
+// Reflect a move in all directions (8 or 4)
+//
+function fold (args = {}) {
+    let move = args . move
+    let out  = [move]
+    let dr   = move . dr
+    let dc   = move . dc
+
+    if (dr) {
+        let copy = {... move}; copy . dr = -dr; copy . dc =  dc;
+        out . push (copy)
+    }
+
+    if (dc) {
+        let copy = {... move}; copy . dr =  dr; copy . dc = -dc;
+        out . push (copy)
+    }
+
+    if (dr && dc) {
+        let copy = {... move}; copy . dr = -dr; copy . dc = -dc;
+        out . push (copy)
+    }
+
+    if (Math . abs (move . dr) != Math . abs (move . dc)) {
+        let n = out . length // We're pushing on out, so we need to
+                             // capture the length before pushing
+        for (let i = 0; i < n; i ++) {
+            let dr = out [i] . dr
+            let dc = out [i] . dc
+            let copy   = {... out [i]}; copy . dr = dc; copy . dc = dr
+            out . push (copy)
+        }
+    }
+    return out
+}
+
 class Piece {
+    constructor (args = {}) {
+        this . move_list = []
+        if (args . move_list) {
+            this . move_list = args . move_list
+        }
+    }
+    moves (args = {}) {
+        return this . move_list
+    }
 }
 
 
 class Knight extends Piece {
-    //
-    // Return a list of moves
-    //
-    moves (args = {}) {
-        return [{type: "step", dr:  2, dc:  1},
-                {type: "step", dr:  2, dc: -1},
-                {type: "step", dr: -2, dc:  1},
-                {type: "step", dr: -2, dc: -1},
-                {type: "step", dr:  1, dc:  2},
-                {type: "step", dr:  1, dc: -2},
-                {type: "step", dr: -1, dc:  2},
-                {type: "step", dr: -1, dc: -2},]
+    constructor (args = {}) {
+        args . move_list = fold ({move: step (1, 2)})
+        super (args)
     }
 }
 
 
 class King extends Piece {
-    //
-    // Return a list of moves
-    //
-    moves (args = {}) {
-        return [{type: "step", dr:  1, dc:  0},
-                {type: "step", dr: -1, dc:  0},
-                {type: "step", dr:  0, dc:  1},
-                {type: "step", dr:  0, dc: -1},]
+    constructor (args = {}) {
+        args . move_list = fold ({move: step (1, 0)})
+        super (args)
     }
 }
 
 
 class Bishop extends Piece {
-    //
-    // Return a list of moves
-    //
-    moves (args = {}) {
-        return [{type: "slide", dr:  1, dc:  1},
-                {type: "slide", dr: -1, dc:  1},
-                {type: "slide", dr:  1, dc: -1},
-                {type: "slide", dr: -1, dc: -1},]
+    constructor (args = {}) {
+        args . move_list = fold ({move: slide (1, 1)})
+        super (args)
     }
 }
