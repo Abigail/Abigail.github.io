dc7bb68ca244cf4272ddbc3a3b84bd3b5510a2f2
Abigail <abigail@abigail.freedom.nl>
2022-06-03 22:55:03 +0200

    Trapped: In the boxset, make "own" piece bold, and not a link.

--- JavaScript/Trapped/boxset.js
+++ JavaScript/Trapped/boxset.js
@@ -13,6 +13,8 @@ function set_up_boxset (div) {
     let set_info = window . set_info
     let sets = $(div) . data ("sets") . split (/,\s*/)
 
+    let this_file  = window . location . href . match (/[^\/]*\.html/) [0]
+
     let table = "<table class = 'boxset'>"
     sets . forEach ((setname) => {
         let info   = set_info [setname]
@@ -21,9 +23,16 @@ function set_up_boxset (div) {
         table += `<tr><th>${title_case (setname)}</th><td>`
         pieces . forEach ((piece_name) => {
             let piece = new Piece ({piece_name: piece_name})
-            table += `<span class = 'piece_name'>` +
-                     `<a href = '${piece . file ()}'>` +
-                     `${piece . name ()}</a></span> `
+            let file  = piece . file () . match (/[^\/]*\.html/) [0]
+
+            table += `<span class = 'piece_name'>`
+            if (this_file == file) {
+                table += `<span class = 'self'>${piece . name ()}</span>`
+            }
+            else {
+                table += `<a href = '${file}'>${piece . name ()}</a>`
+            }
+            table += "</span>"
         })
         table += "</td></tr>"
     })
