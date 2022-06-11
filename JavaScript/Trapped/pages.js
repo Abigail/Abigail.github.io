8676609f0aabdd6c6102654bc5d2737f71651ec7
Abigail <abigail@abigail.freedom.nl>
2022-06-11 20:57:36 +0200

    Trapped: Pages will now use the piece_list.
    So, when navigating with next/prev, we move through all the pieces
    in the same order as defined in the piece_list, and, more importantly,
    we don't have to update pages.js each time we add a piece.

--- JavaScript/Trapped/pages.js
+++ JavaScript/Trapped/pages.js
@@ -4,116 +4,13 @@
 
 let dir   = "Trapped";
 
-let pages = [
-    //
-    // Regular Chess
-    //
-    ["king"],
-    ["queen"],
-    ["rook"],
-    ["bishop"],
-    ["knight"],
-    ["pawn"],
-    //
-    // Basic Leapers
-    //
-    ["wazir"],
-    ["ferz"],
-    ["dabbaba"],
-    ["alfil"],
-    ["threeleaper"],
-    ["camel"],
-    ["zebra"],
-    ["tripper"],
-    ["fourleaper"],
-    ["giraffe"],
-    ["stag"],
-    ["antelope"],
-    ["commuter"],
-    ["flamingo"],
-
-    //
-    // Combined leapers
-    //
-    ["squirrel"],
-    ["caliph"],
-    ["hawk"],
-    ["champion"],
-    ["wizard"],
-    ["gnu"],
-    ["bison"],
-    ["okapi"],
-    ["zebu"],
-    ["root_25_leaper"],
-    ["root_50_leaper"],
-
-    //
-    // Riders
-    //
-    ["dabbabarider"],
-    ["knightrider"],
-    ["alfilrider"],
-    ["threeleaperrider"],
-    ["camelrider"],
-    ["zebrarider"],
-    ["tripperrider"],
-    
-    //
-    // Knighted Pieces
-    //
-    ["archbishop"],
-    ["chancellor"],
-    ["amazon"],
-
-    // 
-    // Pawned pieces
-    //
-    ["dragon"],
-    ["gryphon"],
-
-    //
-    // Shogi
-    //
-    ["dragon_king"],
-    ["dragon_horse"],
-    ["gold_general"],
-    ["silver_general"],
-    ["katsura_horse"],
-    ["incense_chariot"],
-
-    //
-    // Hunter-Falcon chess
-    //
-    ["hunter"],
-    ["falcon"],
-
-    //
-    // Chess variants
-    //
-    ["abbot"],
-    ["acme"],
-    ["acropolis"],
-    ["actor"],
-    ["actress"],
-
-    //
-    // Shogi variants
-    //
-    ["blind_monkey"],
-    ["flying_cock"],
-
-    //
-    // Fairy Chess
-    //
-    ["frog"],
-]
-
-pages . forEach (entry => {
-    if (entry . length == 1) {
-        entry . push (title_case (entry [0]))
+let pages = [[dir + "/" + "index.html", "Index"]]
+for (const piece_name in window . pieces) {
+    let piece = new Piece ({piece_name: piece_name})
+    if (!piece . parent) {
+        pages . push ([dir + "/" + piece . file (), piece . name ()])
     }
-    entry [0]  = dir + "/" + entry [0] + ".html"
-})
+}
 
 window . pages = pages
 window . dir   = dir
