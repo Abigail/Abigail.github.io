013f75ab1af47e6bdce648fc4eaa6db63b67b090
Abigail <abigail@abigail.freedom.nl>
2024-07-29 15:44:12 +0200

    Logic Puzzles: A better way to handle subject/pattern code blocks.
    We now do the bulk with a subject/pattern directive. We only need
    CSS for the font-size (which will vary from page to page), and, for
    now, replacing %%MACROS%%.

--- JavaScript/Talks/Logic_Puzzles/binairo-22.js
+++ JavaScript/Talks/Logic_Puzzles/binairo-22.js
@@ -1,10 +1,7 @@
 $(document) . ready (function () {
-    $("code") . addClass ("pattern")
     let element = $(".content")
     let text = element . html ()
                        . replaceAll ("%%WHITE%%", "&#x25CB;")
                        . replaceAll ("%%BLACK%%", "&#x25CF;")
-                       . replace ("<pre>", "<p>")
-                       . replace ("</pre>", "</p>")
     element . html (text)
 })
